aea524970d19a31b6613089211479fefb92c3e74
Melanie Gaston <melgaston@Melanies-MacBook-Air.local>
2019-02-07 13:34:48 -0500

    mvp navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { AuthComponent } from './auth/auth.component';
+import { ProductsComponent } from './products/products.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch:'full' },
+  { path:'home', component: HomeComponent },
+  { path:'about', component: AboutComponent },
+  { path:'auth', component: AuthComponent },
+  { path:'products', component: ProductsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
